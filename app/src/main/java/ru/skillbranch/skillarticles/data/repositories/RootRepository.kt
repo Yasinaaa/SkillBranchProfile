441aa98921f1aef683617527733eab919e8d3109
Yasina Zalyalova <zalyalovayasina@gmail.com>
2021-03-02 13:59:44 +0300

    add forgotten classes to git

--- app/src/main/java/ru/skillbranch/skillarticles/data/repositories/RootRepository.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/RootRepository.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import ru.skillbranch.skillarticles.data.local.PrefManager
 import ru.skillbranch.skillarticles.data.remote.NetworkManager
 import ru.skillbranch.skillarticles.data.remote.req.LoginReq
-import ru.skillbranch.skillarticles.data.remote.req.RegistrationReq
 
 object RootRepository {
 
@@ -20,10 +19,10 @@ object RootRepository {
         preferences.refreshToken = auth.refreshToken
     }
 
-    suspend fun handleRegister(name: String, login: String, password: String, dest: Int?) {
-        val auth = network.register(RegistrationReq(name, login, password))
-        preferences.profile = auth.user
-        preferences.accessToken = "Bearer ${auth.accessToken}"
-        preferences.refreshToken = auth.refreshToken
-    }
+//    suspend fun handleRegister(name: String, login: String, password: String, dest: Int?) {
+//        val auth = network.register(RegistrationReq(name, login, password))
+//        preferences.profile = auth.user
+//        preferences.accessToken = "Bearer ${auth.accessToken}"
+//        preferences.refreshToken = auth.refreshToken
+//    }
 }
